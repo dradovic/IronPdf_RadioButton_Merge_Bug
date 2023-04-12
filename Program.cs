aa7b719089a06e4c40f696215920ea3c8111852e
Dejan Appenzeller <dejan.appenzeller@swisspeers.ch>
2023-04-12 10:25:43 +0200

    implement repro console app

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using IronPdf;
+
+var renderer = new ChromePdfRenderer();
+var pdf = renderer.RenderHtmlAsPdf("""
+    <div>
+        <p>Do you agree to the terms?</p>
+        <p>
+            <input type="radio" id="yes" name="impact" value="yes">
+            <label for="yes">Ja</label><br>
+        </p>
+        <p>
+            <input type="radio" id="no" name="impact" value="no">
+            <label for="no">Nein</label><br>
+        </p>
+    </div>
+    """);
+pdf.SaveAs("radio.pdf"); // works as expected
+var attachment = new PdfDocument("test.pdf");
+var pdfWithAttachment = PdfDocument.Merge(pdf, attachment);
+pdfWithAttachment.SaveAs("radioWithAttachment.pdf"); // the radio buttons in this pdf are broken
